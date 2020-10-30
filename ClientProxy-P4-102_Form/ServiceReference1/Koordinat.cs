0751baba2c18152e07b96af270a904fa23179877
poetra-As <62783856+poetra-As@users.noreply.github.com>
2020-10-30 11:01:47 +0700

    Add project files.

---
+++ ClientProxy-P4-102_Form/ServiceReference1/Koordinat.cs
@@ -0,0 +1,12 @@
+﻿namespace ServiceReference1
+{
+    internal class Koordinat
+    {
+        public Koordinat()
+        {
+        }
+
+        public int Y { get; internal set; }
+        public int X { get; internal set; }
+    }
+}
---
+++ ClientProxy-P4-102_Form/ServiceReference1/MatematikaClient.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace ServiceReference1
+{
+    internal class MatematikaClient
+    {
+        public MatematikaClient()
+        {
+        }
+
+        internal double Bagi(int v1, int v2)
+        {
+            throw new NotImplementedException();
+        }
+
+        internal double Kali(int v1, int v2)
+        {
+            throw new NotImplementedException();
+        }
+
+        internal double Kurang(int v1, int v2)
+        {
+            throw new NotImplementedException();
+        }
+
+        internal double Tambah(int v1, int v2)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
